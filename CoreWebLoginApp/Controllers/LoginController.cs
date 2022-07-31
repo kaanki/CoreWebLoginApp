6ccdea2e5e2b9b65e5ef18e74932f1a963fa9015
Kaan <Kaan@DESKTOP-KA0QGUR>
2022-07-31 17:00:05 +0300

    Add project files.

---
+++ CoreWebLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            return View();
+        }
+    }
+}
