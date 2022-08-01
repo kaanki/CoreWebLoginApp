3029a5588a085b77e74167b3e758976518c2a801
Kaanki <kaan_9435@hotmail.com>
2022-08-01 17:51:00 +0300

    Login register pages

--- CoreWebLoginApp/Controllers/LoginController.cs
+++ CoreWebLoginApp/Controllers/LoginController.cs
@@ -14,9 +14,32 @@ namespace CoreWebLoginApp.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Login(string username, string password)
+        public async Task<IActionResult> LoginAsync(string username, string password)
         {
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://randomuser.me/api/"),
+                    Headers ={
+                { "X-RapidAPI-Key", "SIGN-UP-FOR-KEY" },
+                { "X-RapidAPI-Host", "random-user-generator1.p.rapidapi.com" },
+            },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(body);
+                }
+            }
+            catch (Exception e)
+            {
+            }
             return View();
         }
+
     }
 }
